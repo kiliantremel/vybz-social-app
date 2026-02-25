f347db590809e7139680502d252f01254a3b0a24
kiliantremel <chaineperso91@gmail.com>
2026-02-25 15:16:15 +0100

    Update page.js
    Vrai code

--- app/page.js
+++ app/page.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>VERSION 2</h1>;
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🔥 VYBZ Social App</h1>
+
+      <p>Bienvenue sur ton application sociale !</p>
+
+      <Link href="/login">
+        <button style={{ marginTop: "20px", padding: "10px 20px" }}>
+          Se connecter
+        </button>
+      </Link>
+    </main>
+  );
 }
