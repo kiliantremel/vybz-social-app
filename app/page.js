dac1ac03337702ebe58354897485830476a14aa7
kiliantremel <chaineperso91@gmail.com>
2026-02-25 15:10:10 +0100

    Update page.js
    Test Version 2

--- app/page.js
+++ app/page.js
@@ -1,23 +1,3 @@
 export default function Home() {
-  return (
-    <div style={{
-      background: "#0f0f0f",
-      color: "white",
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column"
-    }}>
-      <h1>🔥 VYBZ Social App</h1>
-      <p>Ton réseau social nouvelle génération</p>
-    </div>
-  );
+  return <h1>VERSION 2</h1>;
 }
-import Link from "next/link";
-
-<Link href="/login">
-  <button style={{ marginTop: "20px" }}>
-    Se connecter
-  </button>
-</Link>
