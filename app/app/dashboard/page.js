f47fefc17de14056f53f43b1e96879508c09ea5e
kiliantremel <chaineperso91@gmail.com>
2026-02-25 15:59:01 +0100

    Create page.js
    Save

---
+++ app/app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Bienvenue sur ton Dashboard 🎉</h1>
+      <p>Tu es connecté avec succès.</p>
+    </div>
+  );
+}
